e571394c4e166013b1f026ca8ceadad5f5106b30
unknown <you@example.com>
2019-04-08 23:33:56 +0100

    Version 0.3

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 const request = new XMLHttpRequest()
 
-let country = ''
+let city = ''
 
 document.querySelector('#cityName').addEventListener('input', (e) => {
     country = e.target.value
-    console.log(e.target.value)
     getWeather(country)
-} )
-let getWeather = function(country) {
-    request.open('GET', `http://api.apixu.com/v1/forecast.json?key=16394afabc39402891f145202190704&q=${country}`)
-    request.send()
-    request.addEventListener('readystatechange', (e) => {
-        if(e.target.readyState === 4 && e.target.status === 200){
-            const data = JSON.parse(e.target.responseText)
-            showWeather(country, data)
-        }else if(e.target.readyState === 4 && e.target.status !== 200){
-            badRequest()
+})
+
+let getWeather = function(city) {
+    // request.open('GET', `http://api.apixu.com/v1/forecast.json?key=16394afabc39402891f145202190704&q=${city}`)
+    // request.send()
+    // request.addEventListener('readystatechange', (e) => {
+    //     if(e.target.readyState === 4 && e.target.status === 200){
+    //         const data = JSON.parse(e.target.responseText)
+    //         showWeather(country, data)
+    //     }else if(e.target.readyState === 4 && e.target.status !== 200){
+    //         badRequest()
+    //     }
+    // })
+
+    fetch(`http://api.apixu.com/v1/forecast.json?key=16394afabc39402891f145202190704&q=${country}`).then(response => {
+        if(response.status === 200){
+            return response.json()
         }
-    })
+    }).then(data => {
+            document.querySelector('div').innerHTML = ''
+            let p = document.createElement('p')
+            p.textContent = `The weather in ${country} is: ${data.current.feelslike_c}`
+           return document.querySelector('div').appendChild(p)
+    }).catch(() => badRequest())
 }
 
 const badRequest = () => {
@@ -27,9 +38,9 @@ const badRequest = () => {
     document.querySelector('div').appendChild(p)
 }
 
-const showWeather = (country, data) => {
-    document.querySelector('div').innerHTML = ''
-    let p = document.createElement('p')
-    p.textContent = `The weather in ${country} is: ${data.current.feelslike_c}`
-    document.querySelector('div').appendChild(p)
-}
+// const showWeather = (country, data) => {
+//     document.querySelector('div').innerHTML = ''
+//     let p = document.createElement('p')
+//     p.textContent = `The weather in ${country} is: ${data.current.feelslike_c}`
+//     document.querySelector('div').appendChild(p)
+// }
