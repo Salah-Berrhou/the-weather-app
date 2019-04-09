2db12308826c25a518de74bd155a3d4413351552
unknown <you@example.com>
2019-04-09 21:55:39 +0100

    version 0.6

--- app.js
+++ app.js
@@ -3,30 +3,19 @@ const request = new XMLHttpRequest()
 let city = ''
 
 document.querySelector('#cityName').addEventListener('input', (e) => {
-    country = e.target.value
-    getWeather(country)
+    city = e.target.value
+    getWeather(city)
 })
 
 let getWeather = function(city) {
-    // request.open('GET', `http://api.apixu.com/v1/forecast.json?key=16394afabc39402891f145202190704&q=${city}`)
-    // request.send()
-    // request.addEventListener('readystatechange', (e) => {
-    //     if(e.target.readyState === 4 && e.target.status === 200){
-    //         const data = JSON.parse(e.target.responseText)
-    //         showWeather(country, data)
-    //     }else if(e.target.readyState === 4 && e.target.status !== 200){
-    //         badRequest()
-    //     }
-    // })
-
-    fetch(`http://api.apixu.com/v1/forecast.json?key=16394afabc39402891f145202190704&q=${country}`).then(response => {
+    fetch(`http://api.apixu.com/v1/forecast.json?key=16394afabc39402891f145202190704&q=${city}`).then(response => {
         if(response.status === 200){
             return response.json()
         }
     }).then(data => {
             document.querySelector('div').innerHTML = ''
             let p = document.createElement('p')
-            p.textContent = `The weather in ${country} is: ${data.current.feelslike_c}`
+            p.textContent = `The weather in ${city} is: ${data.forecast.forecastday[0].day.maxtemp_c}C`
            return document.querySelector('div').appendChild(p)
     }).catch(() => badRequest())
 }
@@ -38,9 +27,3 @@ const badRequest = () => {
     document.querySelector('div').appendChild(p)
 }
 
-// const showWeather = (country, data) => {
-//     document.querySelector('div').innerHTML = ''
-//     let p = document.createElement('p')
-//     p.textContent = `The weather in ${country} is: ${data.current.feelslike_c}`
-//     document.querySelector('div').appendChild(p)
-// }
