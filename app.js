126a60a62d4a9b7964f9ee5edc2b6f5256443094
unknown <you@example.com>
2019-04-09 22:09:34 +0100

    version 0.2.1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.querySelector('#cityName').addEventListener('input', (e) => {
 })
 
 let getWeather = function(city) {
-    fetch(`http://api.apixu.com/v1/forecast.json?key=16394afabc39402891f145202190704&q=${city}`).then(response => {
+    fetch(`https://api.apixu.com/v1/forecast.json?key=16394afabc39402891f145202190704&q=${city}`).then(response => {
         if(response.status === 200){
             return response.json()
         }
